7073b35f8151482b3486423e8fc92c8b444730bf
jarnopk <jarnopk@metropolia.fi>
2022-03-04 10:34:42 +0200

    Comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-import {Button, Form, InputGroup, Table} from 'react-bootstrap';
+import {
+  Button,
+  Form,
+  FormText,
+  InputGroup,
+  Modal,
+  Table,
+} from 'react-bootstrap';
 
 
 const Home = () => {
-  const [title, setTitle] = useState('')
-  const [file, setFile] = useState()
-  const [description, setDescription] = useState('')
-  const [images, setImages] = useState([])
+  const [title, setTitle] = useState('');
+  const [file, setFile] = useState();
+  const [description, setDescription] = useState('');
+  const [images, setImages] = useState([]);
+  const [show, setShow] = useState(false);
+  const [comments, setComments] = useState([]);
+  const [commentData, setCommentdata] = useState('');
+  const [testID, setTestID] = useState([]);
+
+  const handleClose = () => setShow(false);
 
 
 
@@ -15,38 +28,38 @@ const Home = () => {
 
     console.log(title, file, description)
     console.log(file[0])
-  /*
-    await fetch('http://localhost:8081/upload', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        file: file,
-        postTitle: title,
-        postDescription: description,
+    /*
+      await fetch('http://localhost:8081/upload', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          file: file,
+          postTitle: title,
+          postDescription: description,
+        })
       })
-    })
-        .then(res => res.json())
+          .then(res => res.json())
 
-*/
+  */
     const formData = new FormData();
-      formData.append('file', file[0]);
-      formData.append('postTitle', title);
-      formData.append('postDescription', description);
-/*
-            const dataToSend = {
-               file: file[0],
-               postTitle: title,
-               postDescription: description,
-            }
-*/
-      //console.log(formData.get('file'));
-     // console.log(formData.get('postTitle'))
+    formData.append('file', file[0]);
+    formData.append('postTitle', title);
+    formData.append('postDescription', description);
+    /*
+                const dataToSend = {
+                   file: file[0],
+                   postTitle: title,
+                   postDescription: description,
+                }
+    */
+    //console.log(formData.get('file'));
+    // console.log(formData.get('postTitle'))
 
-            axios.post('http://localhost:8081/upload', formData )
+    axios.post('http://localhost:8081/upload', formData )
 
-                .then(function(response) {
-                  console.log(response)
-                })
+        .then(function(response) {
+          console.log(response)
+        })
 
         .catch(function(error) {
           console.log(error)
@@ -55,20 +68,55 @@ const Home = () => {
   }
 
 
-    useEffect(() => {
-      console.log("testi");
-      axios.get('http://localhost:8081/images')
-          .then(function (res) {
-            console.log(res.data);
-            const imageData = res.data;
-            setImages(imageData);
-          })
+  useEffect((postID, commentdata) => {
+    console.log("testi");
+    axios.get('http://localhost:8081/images')
+        .then(function (res) {
+          console.log(res.data);
+          const imageData = res.data;
+          setImages(imageData);
+        })
 
 
-    }, [])
+  }, [])
+
+  const getComments = (postID) => {
+    console.log("testi");
+    console.log(postID);
+    setTestID(postID);
+    axios.get('http://localhost:8081/comments?postID=' +postID)
+        .then(function (res) {
+          console.log(res.data);
+          const commentsData = res.data;
+          setComments(commentsData);
+        })
 
 
-    const handleSubmit = (event) => {
+  }
+
+  const addComment = (postID, commentData) => {
+    console.log("testi");
+
+    axios.post('http://localhost:8081/addcomment?postID='+postID+"&commentField="+commentData)
+        .then(function (res) {
+          console.log("This"+res.data);
+
+        })
+
+
+  }
+  const handleCommentSubmit = (event) => {
+    const form = event.currentTarget;
+    console.log("here");
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    addComment(testID, commentData);
+  }
+
+
+  const handleSubmit = (event) => {
     const form = event.currentTarget;
     if(form.checkValidity() === false) {
       event.preventDefault();
@@ -76,14 +124,15 @@ const Home = () => {
     }
 
     addImage(file, title, description);
-    }
-    console.log(images);
-   // console.log(images[4].ID)
+  }
+
+  console.log(images);
+  // console.log(images[4].ID)
   return (
       <div className="container">
         <Form encType="multipart/form-data" onSubmit={handleSubmit} >
           <Form.Group>
-          <Form.Label>Title:</Form.Label>
+            <Form.Label>Title:</Form.Label>
 
             <Form.Control
                 type="text"
@@ -101,7 +150,7 @@ const Home = () => {
                 onChange={(e => setFile(e.target.files))}
                 required/>
           </Form.Group>
-          <Form.Label>Paikka:</Form.Label>
+          <Form.Label>Kuvaus:</Form.Label>
           <Form.Group>
             <Form.Control
                 type="text"
@@ -117,24 +166,49 @@ const Home = () => {
 
 
         </Form>
-      <div>
-        <Table >
-          <tbody>{images.map((image, i) => {
-          return [
-            <tr key={image.id}>
-              <td>
-                <img src={""+image.imagelink} alt={image.title}/>
-                <p>{image.description}</p>
-              </td>
-            </tr>
+        <div>
+          <Table >
+            <tbody>{images.map((image, i) => {
+              return [
+                <tr key={image.id}>
+                  <td>
+                    <img src={""+image.imagelink} alt={image.title}/>
+                    <p>{image.description}</p>
+                    <Button onClick={() => {setShow(true); getComments(image.ID)}} >Show comments {image.ID}</Button>
+                  </td>
+                </tr>
               ];
             })}
+            </tbody>
+          </Table>
+        </div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Comments</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{comments.map((comment, i) => {
+            return [
+              <tr key={comment.id}>
+                <td>{comment.time} : {comment.commentdata}</td>
+
+              </tr>
+            ];
+          })}
+          </Modal.Body>
+          <Modal.Footer >
+            <p>{testID} Text</p>
+            <Form encType="multipart/form-data" onSubmit={handleCommentSubmit}>Add comment
+              <Form.Group >
+                <Form.Control type="text" value={commentData} onChange={(e => setCommentdata(e.target.value))}></Form.Control>
+
+              </Form.Group>
+              <Button size="sm" type="submit">Add</Button>
+            </Form>
+
+          </Modal.Footer>
 
 
-
-          </tbody>
-        </Table>
-      </div>
+        </Modal>
 
       </div>
   )
